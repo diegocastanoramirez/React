a8eb2e142a281251efc002627af7e34c54230d1b
205 <205@academicos.cesde.edu.co>
2022-10-26 20:43:39 -0500

    React inicio

---
+++ src/Componente1/Componente.js
@@ -0,0 +1,7 @@
+export function Componente1(){
+    return(
+        <h1>HOLA SALUDANDO DESDE EL COMPONENTE</h1>
+    )
+}
+
+
---
+++ src/Diego/Diego.js
@@ -0,0 +1,10 @@
+export function Diego(){
+
+    return(
+        <>
+            <h2>HOLA DIEGO</h2>
+            <h1>GANANDO EL CURSO</h1>
+            
+        </>
+    )
+}
